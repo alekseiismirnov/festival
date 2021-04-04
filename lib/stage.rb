6de4915132e3586262291a4ac5b0b8eb3a30f635
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-04-04 06:55:43 +0300

    add some style

--- lib/stage.rb
+++ lib/stage.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
+# Just has many artists. Should has backend for CRUD requests.
 class Stage
   @@stages = []
 
   def self.clear; end
 
   def self.all
-    return @@stages
+    @@stages
   end
 end
