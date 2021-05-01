d7b22dc02f1cf2372a9796fb7b49c666c7c9be2e
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-05-01 23:01:32 +0300

    add .find_by_stage stage_id

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 # Belongs to the one Stage, CRUD
 class Artist
-  attr_reader :name, :id
+  attr_reader :name, :id, :stage_id
 
   @artists = {}
   @total_rows = 0
@@ -12,6 +12,7 @@ class Artist
   def initialize(artist_name, id = nil)
     @name = artist_name
     @id = id || self.class.free_id
+    @stage_id = nil
   end
 
   def self.free_id
@@ -23,7 +24,7 @@ class Artist
   end
 
   def self.add_artist(artist)
-    @artists[artist.id] = Artist.new(artist.name, artist.id)
+    @artists[artist.id] = artist
   end
 
   def self.all
@@ -34,11 +35,23 @@ class Artist
     @artists[id]
   end
 
+  def self.find_by_stage(stage_id)
+    @artists.values.select do |artist|
+      artist.stage_id == stage_id
+    end
+  end
+
   def ==(other)
     name == other.name
   end
 
   def save
-    Artist.add_artist self
+    artist = Artist.new(name, id)
+    artist.assign_to_stage stage_id
+    Artist.add_artist artist
+  end
+
+  def assign_to_stage(stage_id)
+    @stage_id = stage_id
   end
 end
