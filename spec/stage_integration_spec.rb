2a733f5f3442f3268487a08da385e8c583112c96
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-05-04 03:49:45 +0300

    remove redundant :before operations

--- spec/stage_integration_spec.rb
+++ spec/stage_integration_spec.rb
@@ -6,23 +6,11 @@ set(:show_exceptions, false)
 
 describe('Stages page requirements', type: :feature) do
   before :each do
-    Artist.clear
-    @artist1 = Artist.new('First Artist Name')
-    @artist2 = Artist.new('Second Artist Name')
-    @artist1.save
-    @artist2.save
-
+    Stage.clear
     @stage = Stage.new('All Numbers')
     @stage.save
     @stage2 = Stage.new('Live me alone')
     @stage2.save
-    @artists_list = []
-    (1..10).each do |i|
-      artist = Artist.new("Artist #{i}")
-      artist.assign_to_stage @stage.id
-      artist.save
-      @artists_list << artist
-    end
     visit '/stages'
   end
 
